7d489a8a0f07617fb946ec71475534f5335a0490
Siddharth Gargate <siddharth.gargate@springernature.com>
2019-06-19 14:55:37 +0530

    fixed issue with index

--- src/test/kotlin/SelectionSortTest.kt
+++ src/test/kotlin/SelectionSortTest.kt
@@ -18,12 +18,17 @@ class SelectionSortTest {
         assertEquals(listOf(1,2,3), selectionSort(listOf(3,2,1)))
     }
 
+    @Test fun testRandomNumbers(){
+        assertEquals(listOf(1,2,3,4,5,6,7,8,9), selectionSort(listOf(7,6,8,2,1,3,4,9,5)))
+    }
+
     private fun selectionSort(list: List<Int>): List<Int> {
         val mutableList = list.toMutableList()
         (0 until mutableList.size).map {
             val min = mutableList.subList(it, mutableList.size).withIndex().minBy { v->v.value }
+
             if (mutableList[it] > min!!.value) {
-                mutableList.swap(it, min.index)
+                mutableList.swap(it, min.index+it)
             }
         }
         return mutableList.toList()
